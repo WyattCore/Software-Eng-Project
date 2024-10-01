a8708e57f67d193c54ffe931adb316465f75b87a
Axyl Liang <166651410+axyliang@users.noreply.github.com>
2024-10-01 13:06:54 -0500

    Update user.py

--- src/user.py
+++ src/user.py
@@ -2,20 +2,23 @@ class User:
     row: int 
     equipment_id: int
     user_id: int
-    username: str
+    codename: str  # Updated from username to codename
     game_score: int
     has_hit_base: bool
     
-    # Passing in row of entry from GUI, equipment ID, user ID, and username
-    def __init__(self, row, equipment_id, user_id, username) -> None:
+    # Passing in row of entry from GUI, equipment ID, user ID, and codename
+    def __init__(self, row: int, equipment_id: int, user_id: int, codename: str) -> None:
         self.row = row
         self.equipment_id = equipment_id
         self.user_id = user_id
-        self.username = username
+        self.codename = codename  # Updated from username to codename
         self.game_score = 0
         self.has_hit_base = False
 
     # String representation of User object
     def __str__(self) -> str:
-        return f"Username: {self.username}\nEquipment ID: {self.equipment_id}\nUser ID: {self.user_id}\nGame Score: {self.game_score}\nHas Hit Base: {self.has_hit_base}\n\n"
-    
+        return (f"Codename: {self.codename}\n"  # Updated from username to codename
+                f"Equipment ID: {self.equipment_id}\n"
+                f"User ID: {self.user_id}\n"
+                f"Game Score: {self.game_score}\n"
+                f"Has Hit Base: {self.has_hit_base}\n\n")
